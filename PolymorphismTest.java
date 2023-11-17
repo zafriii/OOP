be893678bfa8528b6c0cc15ca6ebfaf4a7f10908
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2023-11-17 21:00:23 +0600

    Update PolymorphismTest.java

--- PolymorphismTest.java
+++ PolymorphismTest.java
@@ -1,10 +1,10 @@
 public class PolymorphismTest {
     public static void main(String[] args) {
         CommissionEmployee commissionEmployee = new CommissionEmployee(
-                "Safa", "chowdhury", "221-134-011", 10000, .06);
+                "Niha", "Zafar", "221-134-011", 10000, .06);
 
         BasePlusCommissionEmployee BasePlusCommissionEmployee = new BasePlusCommissionEmployee(
-                "Safa", "chowdhury", "221-134-011", 10000, .06, 300);
+                "Niha", "Zafar", "221-134-011", 10000, .06, 300);
 
         System.out.printf("%s %s:%n%n%s%n%n", "Call Commission's toString with superclass reference ",
                 "to superclass object", commissionEmployee.toString());
